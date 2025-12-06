c95432d753e4c30774d19499204cfc3e46721572
VladimirSagundo <roger.sagundo.cardena@gmail.com>
2025-12-05 19:34:47 -0600

    Commit actividades backend

--- moma-backend/actividades/views.py
+++ moma-backend/actividades/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Actividades
-
+#Actividades Vladimir
 class ActividadesListView(APIView):
     def get(self, request):
         # FILTRO IMPORTANTE: Solo traemos las actividades con activo=1
